99cf0380edaa82411ffb02e9458ff1e7a6ca5eb5
Leandro <le.chaves.silva@gmail.com>
2021-11-24 20:13:05 -0300

    correcao

--- horarios/forms.py
+++ horarios/forms.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django import forms
+from django.forms import widgets
 from .models import Agenda
+from clientes.models import Cliente
 from datetime import date
 
 
@@ -14,6 +16,11 @@ class AgendaForm(forms.ModelForm):
         label='Horário',
         widget=forms.DateInput(
             format='%H:%M', attrs={'type': 'time'}),
+    )
+    cliente = forms.ModelChoiceField(
+        label='Cliente',
+        queryset=Cliente.objects.all().order_by('nome'),
+        required=True,        
     )
     class Meta:
         model = Agenda
